926e58f8ad77465d97dc4fbc0cf907f0ce4f0861
hadlinar <hadlinar@gmail.com>
2023-03-09 11:37:09 +0700

    changed fitering for so (date, month, year)

--- controller/PN.js
+++ controller/PN.js
@@ -1,8 +1,17 @@
 const db = require('../config/database.js');
 
 class PN {
-    async getPN(tanggal){
-        let results = await db.query(`SELECT * FROM "vw_pn_otsuka" WHERE tanggal = $1`, [tanggal]).catch(console.log);
+    async getPNDate(date){
+        let results = await db.query(`SELECT * FROM "vw_pn_otsuka" WHERE tanggal = $1`, [date]).catch(console.log);
+        return results.rows;
+    };
+
+    async getPNMonth(month){
+        let results = await db.query(`SELECT * FROM "vw_pn_otsuka" WHERE per_month = $1`, [month]).catch(console.log);
+        return results.rows;
+    };
+    async getPNYear(year){
+        let results = await db.query(`SELECT * FROM "vw_pn_otsuka" WHERE per_year = $1`, [year]).catch(console.log);
         return results.rows;
     };
 
--- route/PN.js
+++ route/PN.js
@@ -4,7 +4,32 @@ const PN = require('../controller/PN');
 
 router.get(`/otsuka/so`, async (req,res) => {
     let date = req.query.date
-    let pn = await new PN().getPN(date)
+    let month = req.query.month
+    let year = req.query.year
+    let tgl
+
+    if(date !=  "") {
+        tgl = `${year}-${month}-${date}`
+        let pn = await new PN().getPNDate(tgl)
+        return res.status(200).json({
+            "result": pn
+        })
+    } 
+    else if(date == "" && month != "") {
+        let pn = await new PN().getPNMonth(month)
+        return res.status(200).json({
+            "result": pn
+        })
+    }
+    else if(date == "" && month == "" && year != "") {
+        let pn = await new PN().getPNYear(year)
+        return res.status(200).json({
+            "result": pn
+        })
+    }
+
+
+    let pn = await new PN().getPNDate(date)
     return res.status(200).json({
         "result": pn
     })
